e2c803c3db9aef9a88869fce84942fc872e3416b
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-01-13 21:03:46 -0400

    Agregando Lista de Tecnicos agregados

--- app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
@@ -6,8 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -15,7 +19,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.room.Dao
 import androidx.room.Database
 import androidx.room.Delete
@@ -59,7 +65,25 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    private fun TicketRow(it: TechnicianEntity) {
+    fun TechnicianListScreen(techList: List<TechnicianEntity>){
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.height(32.dp))
+            Text("Lista de Técnicos")
+
+            LazyColumn(
+                modifier =  Modifier.fillMaxSize()
+            ) {
+                items(techList){
+                    TechnicianRow(it)
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun TechnicianRow(it: TechnicianEntity) {
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
