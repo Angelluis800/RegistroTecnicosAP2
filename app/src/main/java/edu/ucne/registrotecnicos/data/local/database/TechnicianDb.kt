04f9ffd1d28b690041716e5c909fc4f52870c34a
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-01-17 11:47:04 -0400

    Creando TecnicoDatabase

---
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TechnicianDb.kt
@@ -0,0 +1,16 @@
+package edu.ucne.registrotecnicos.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.registrotecnicos.data.local.dao.TechnicianDao
+import edu.ucne.registrotecnicos.data.local.entity.TechnicianEntity
+
+@Database(
+        entities = [
+            TechnicianEntity::class
+        ],
+        version = 1,
+        exportSchema = false
+    )abstract class TechnicianDb : RoomDatabase() {
+        abstract fun technicianDao(): TechnicianDao
+    }
