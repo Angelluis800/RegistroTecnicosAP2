2d9448c3ac959f047452dac8f9cf8889b04079f1
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-01-17 11:48:58 -0400

    Creando Pantalla de la creacion de un Tecnico

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/technician/TechnicianScreen.kt
@@ -0,0 +1,129 @@
+package edu.ucne.registrotecnicos.presentation.technician
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.registrotecnicos.data.local.entity.TechnicianEntity
+import kotlinx.coroutines.launch
+
+@Composable
+    fun TechnicianScreen(
+    ){
+        var name by remember { mutableStateOf("") }
+        var salary by remember { mutableStateOf("") }
+        var errorMessage: String? by remember { mutableStateOf(null) }
+
+        Scaffold { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(8.dp)
+            ) {
+                ElevatedCard(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        OutlinedTextField(
+                            label = { Text(text = "Nombre")},
+                            value = name,
+                            onValueChange = { name = it},
+                            modifier =  Modifier.fillMaxWidth()
+                        )
+                        OutlinedTextField(
+                            label = { Text(text = "Sueldo") },
+                            value = salary,
+                            onValueChange = { salary = it },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer( modifier = Modifier.padding(2.dp))
+                        errorMessage?.let {
+                            Text(text = it, color = Color.Red)
+                        }
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            OutlinedButton(
+                                onClick = {
+                                    name = ""
+                                    salary = ""
+                                    errorMessage = ""
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = "new button"
+                                )
+                                Text(text = "Nuevo")
+                            }
+                            val scope = rememberCoroutineScope()
+                            OutlinedButton(
+                                onClick = {
+                                    if(name.isBlank())
+                                        errorMessage = "Nombre Vacio"
+
+                                    scope.launch {
+                                        saveTechnician(
+                                            TechnicianEntity(
+                                                name = name,
+                                                salary = salary.toDouble()
+                                            )
+                                        )
+                                        name = ""
+                                        salary = ""
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Edit,
+                                    contentDescription = "save button"
+                                )
+                                Text(text = "Guardar")
+                            }
+                        }
+                    }
+                }
+
+                val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner.current
+                val technicianList by technicianDb.technicianDao().getAll()
+                    .collectAsStateWithLifecycle(
+                        initialValue = emptyList(),
+                        lifecycleOwner = lifecycleOwner,
+                        minActiveState = Lifecycle.State.STARTED
+                    )
+                TechnicianListScreen(technicianList)
+            }
+        }
+    }
