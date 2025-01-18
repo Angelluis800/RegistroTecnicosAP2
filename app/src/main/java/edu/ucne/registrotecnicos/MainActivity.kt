6831986aaf8ad7daade8390aa3cfae0638c5532a
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-01-18 07:05:04 -0400

    Actualizando el MainAct
    1. Usando el Repository del Técnico
    2.Ahora el technicianDb es un val
    3.Utilizando el NavHost

--- app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
@@ -4,90 +4,52 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.room.Dao
-import androidx.room.Database
-import androidx.room.Delete
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import androidx.room.Query
+import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.room.Upsert
 import edu.ucne.registrotecnicos.data.local.database.TechnicianDb
 import edu.ucne.registrotecnicos.data.local.entity.TechnicianEntity
-import edu.ucne.registrotecnicos.presentation.technician.TechnicianScreen
+import edu.ucne.registrotecnicos.data.repository.TecnicoRepository
+import edu.ucne.registrotecnicos.presentation.navigation.TecnicoNavHost
 import edu.ucne.registrotecnicos.ui.theme.RegistroTecnicosTheme
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    private lateinit var technicianDb: TechnicianDb
+    private lateinit var tecnicoRepository: TecnicoRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        technicianDb = Room.databaseBuilder(
+        val technicianDb = Room.databaseBuilder(
             applicationContext,
             TechnicianDb::class.java,
             "TechnicianDb"
         ).fallbackToDestructiveMigration()
             .build()
+        tecnicoRepository = TecnicoRepository(technicianDb)
 
         setContent {
             RegistroTecnicosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                    ) {
-                        TechnicianScreen()
-                    }
-                }
-
+                val navHost = rememberNavController()
+                TecnicoNavHost(navHost,tecnicoRepository)
             }
         }
     }
 
-
-    private suspend fun saveTechnician(technician: TechnicianEntity) {
-        technicianDb.technicianDao().save(technician)
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun TechPreview() {
+        RegistroTecnicosTheme {
+            val tecnicoList = listOf(
+                TechnicianEntity(1, "Angel", 15000.0)
+            )
+        }
     }
 }
 
