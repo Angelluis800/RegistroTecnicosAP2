71bff18cb65c7729d6ce77fae2262a4c6432d8c3
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-01-18 10:48:24 -0400

    Agregando la entidad ticket y la funcion ticketDao en la clase Database

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/database/AppDataDb.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/AppDataDb.kt
@@ -3,14 +3,18 @@ package edu.ucne.registrotecnicos.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.registrotecnicos.data.local.dao.TechnicianDao
+import edu.ucne.registrotecnicos.data.local.dao.TicketDao
 import edu.ucne.registrotecnicos.data.local.entity.TechnicianEntity
+import edu.ucne.registrotecnicos.data.local.entity.TicketEntity
 
 @Database(
         entities = [
-            TechnicianEntity::class
+            TechnicianEntity::class,
+            TicketEntity::class
         ],
-        version = 1,
+        version = 2,
         exportSchema = false
     )abstract class AppDataDb : RoomDatabase() {
         abstract fun technicianDao(): TechnicianDao
+        abstract fun ticketDao(): TicketDao
     }
