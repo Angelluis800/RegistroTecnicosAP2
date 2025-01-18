9deeac19c759d25649d5397b8dfa272723b0462a
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-01-18 16:52:35 -0400

    Agregando TicketList, Ticket y HomeScreen

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
@@ -3,9 +3,21 @@ package edu.ucne.registrotecnicos.presentation.navigation
 import kotlinx.serialization.Serializable
 
 sealed class Screen {
+    //Técnicos
     @Serializable
     data object TechnicianList : Screen() //Consulta
 
     @Serializable
     data class Technician(val technicianId: Int) : Screen() //Registro
+
+    //Tickets
+
+    @Serializable
+    data object TicketList : Screen() //Consulta
+
+    @Serializable
+    data class Ticket(val ticketId: Int) : Screen() //Registro
+
+    @Serializable
+    data object HomeScreen : Screen() //Pantalla de Inicio
 }
