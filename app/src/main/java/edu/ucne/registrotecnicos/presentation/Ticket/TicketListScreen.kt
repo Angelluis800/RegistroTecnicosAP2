b2812da74dc87de297859c1f296acb44b9ee19e5
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-01-22 18:45:52 -0400

    Modificando valores del padding

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/Ticket/TicketListScreen.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/Ticket/TicketListScreen.kt
@@ -1,5 +1,6 @@
 package edu.ucne.registrotecnicos.presentation.Ticket
 
+import android.graphics.Outline
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -125,8 +126,8 @@ private fun TicketRow(
                 }
             }
             .padding(
-                horizontal = 3.dp,
-                vertical = 2.dp
+                horizontal = 4.dp,
+                vertical = 4.dp
             )
     ) {
         Text(
