4890b01eb4ae6ebdac14598c735e07a814f4fb31
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-01-25 16:03:03 -0400

    Cambiando el tipo de dato de la fecha

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/entity/TicketEntity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/entity/TicketEntity.kt
@@ -2,12 +2,13 @@ package edu.ucne.registrotecnicos.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.Date
 
 @Entity(tableName = "Tickets")
 data class TicketEntity(
     @PrimaryKey
     val ticketId: Int? = null,
-    val fecha: String = "",
+    val fecha: Date? = null,
     val prioridadId: Int = 0,
     val cliente: String = "",
     val asunto: String = "",
