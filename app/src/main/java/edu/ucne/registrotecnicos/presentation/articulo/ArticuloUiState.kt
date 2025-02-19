4a880f536aa3948f1cae021948dda1cda2bbbfcd
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-19 16:39:07 -0400

    Creando ArticuloUiState

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/articulo/ArticuloUiState.kt
@@ -0,0 +1,15 @@
+package edu.ucne.registrotecnicos.presentation.articulo
+
+import edu.ucne.registrotecnicos.data.remote.dto.ArticuloDto
+
+data class ArticuloUiState(
+    val articuloId: Int = 0,
+    val descripcion: String = "",
+    val costo: Double = 0.0,
+    val ganancia: Double = 0.0,
+    val precio: Double = 0.0,
+    val errorMessage: String? = null,
+    val listaArticulos: List<ArticuloDto> = emptyList(),
+    val isLoading: Boolean = false
+
+)
