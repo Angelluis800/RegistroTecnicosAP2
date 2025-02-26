1bf7ee7633993e6f99af7cd35d9093a41253d725
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-26 03:24:43 -0400

    Modificando la funcion getAllArticulos para que permita guardar los datos del api en la base de datos

--- app/src/main/java/edu/ucne/registrotecnicos/data/repository/ArticuloRepository.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/repository/ArticuloRepository.kt
@@ -1,39 +1,55 @@
 package edu.ucne.registrotecnicos.data.repository
 
 import android.util.Log
+import edu.ucne.registrotecnicos.data.local.dao.ArticuloDao
+import edu.ucne.registrotecnicos.data.local.entity.ArticuloEntity
 import edu.ucne.registrotecnicos.data.remote.RemoteDataSource
 import edu.ucne.registrotecnicos.data.remote.Resource
 import edu.ucne.registrotecnicos.data.remote.dto.ArticuloDto
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
 
 class ArticuloRepository @Inject constructor(
+    private val articuloDao: ArticuloDao,
     private val remoteDataSource: RemoteDataSource
 ) {
 
-    fun getAllArticulos(): Flow<Resource<List<ArticuloDto>>> = flow {
+    fun getAllArticulos(): Flow<Resource<List<ArticuloEntity>>> = flow {
+        emit(Resource.Loading())
+
         try {
-            emit(Resource.Loading())
-            val response = remoteDataSource.getArticulos()
-            if (response.isSuccessful) {
-                response.body()?.let {
-                    emit(Resource.Success(it))
-                } ?: emit(Resource.Error("La respuesta está vacía"))
-            } else {
-                emit(Resource.Error("Error: ${response.code()} - ${response.message()}"))
+            val articuloRemoto = remoteDataSource.getArticulos()
+
+            val listaArticulo = articuloRemoto.map { dto ->
+                ArticuloEntity(
+                    articuloId = dto.itemId,
+                    descripcion = dto.description,
+                    costo = dto.cost,
+                    ganancia = dto.revenue,
+                    precio = dto.price
+                )
             }
-        } catch (e: IOException) {
-            Log.e("ArticuloRepository", "IOException: ${e.message}")
-            emit(Resource.Error("Error de conexión, verifica tu Internet"))
+            articuloDao.save(listaArticulo)
+
+            emit(Resource.Success(listaArticulo))
+
         } catch (e: HttpException) {
-            Log.e("ArticuloRepository", "HttpException: ${e.message()}")
-            emit(Resource.Error("Error HTTP ${e.code()}: ${e.message()}"))
+            val errorMessage = e.response()?.errorBody()?.string() ?: e.message()
+            Log.e("ArticuloRepository", "HttpException: $errorMessage")
+            emit(Resource.Error("Error de conexión: $errorMessage"))
         } catch (e: Exception) {
-            Log.e("ArticuloRepository", "Exception: ${e.message}")
-            emit(Resource.Error("Error inesperado: ${e.message}"))
+            Log.e("ArticuloRepository", "Error al obtener datos remotos: ${e.message}")
+
+            val localArticulos = articuloDao.getAll().first()
+
+            if (localArticulos.isNotEmpty())
+                emit(Resource.Success(localArticulos))
+            else
+                emit(Resource.Error("Error de conexión: ${e.message}"))
         }
     }
 
