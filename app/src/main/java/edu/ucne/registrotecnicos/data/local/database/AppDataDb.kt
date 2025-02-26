eda4131d22fb172717e172a09e38418cf854dbc6
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-26 03:22:22 -0400

    Agregando Articulo en la base de datos local

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/database/AppDataDb.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/AppDataDb.kt
@@ -2,9 +2,11 @@ package edu.ucne.registrotecnicos.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.registrotecnicos.data.local.dao.ArticuloDao
 import edu.ucne.registrotecnicos.data.local.dao.MensajeDao
 import edu.ucne.registrotecnicos.data.local.dao.TechnicianDao
 import edu.ucne.registrotecnicos.data.local.dao.TicketDao
+import edu.ucne.registrotecnicos.data.local.entity.ArticuloEntity
 import edu.ucne.registrotecnicos.data.local.entity.MensajeEntity
 import edu.ucne.registrotecnicos.data.local.entity.TechnicianEntity
 import edu.ucne.registrotecnicos.data.local.entity.TicketEntity
@@ -13,12 +15,14 @@ import edu.ucne.registrotecnicos.data.local.entity.TicketEntity
         entities = [
             TechnicianEntity::class,
             TicketEntity::class,
-            MensajeEntity::class
+            MensajeEntity::class,
+            ArticuloEntity::class
         ],
-        version = 5,
+        version = 6,
         exportSchema = false
     )abstract class AppDataDb : RoomDatabase() {
         abstract fun technicianDao(): TechnicianDao
         abstract fun ticketDao(): TicketDao
         abstract fun mensajeDao(): MensajeDao
+        abstract fun articuloDao(): ArticuloDao
     }
