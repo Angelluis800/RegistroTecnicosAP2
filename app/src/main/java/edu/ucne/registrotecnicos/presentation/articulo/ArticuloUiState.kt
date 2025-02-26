f5186fa19fd94755a51002cec3a98458629520d9
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-26 03:23:09 -0400

    Cambiando el listado de Dto a Entity

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/articulo/ArticuloUiState.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/articulo/ArticuloUiState.kt
@@ -1,5 +1,6 @@
 package edu.ucne.registrotecnicos.presentation.articulo
 
+import edu.ucne.registrotecnicos.data.local.entity.ArticuloEntity
 import edu.ucne.registrotecnicos.data.remote.dto.ArticuloDto
 
 data class ArticuloUiState(
@@ -9,7 +10,7 @@ data class ArticuloUiState(
     val ganancia: Double = 0.0,
     val precio: Double = 0.0,
     val errorMessage: String? = null,
-    val listaArticulos: List<ArticuloDto> = emptyList(),
+    val listaArticulos: List<ArticuloEntity> = emptyList(),
     val isLoading: Boolean = false
 
 )
